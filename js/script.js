e2110a97634dbfcd94078f022128ca8e90c07d4f
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-03-25 22:04:15 -0700

    defined current time function, started box color function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 // Get current date
 //  - append to header
 // 
+
+function showActualTime() {
+    $('#currentDay').text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+}
+
+setInterval(showActualTime, 1000);
+
 // make boxes color
 //  - based on current time
 //
+function currentTimeBox() {
+    if (moment().format("HH") > 0) {
+    console.log('ok');
+    
+    return;
+}};
+
+setInterval(currentTimeBox(), 1000);
 // Make buttons work
 //  -write input to localstorage
-// 
+
 // Figure out local storage pairs
 //  -
-// 
-// 
-// 
-// 
-// 
-// 
-// 
