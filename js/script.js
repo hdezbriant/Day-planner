8f5a285e0244f573a9e362984328a4fffcc4d406
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-03-25 23:14:54 -0700

    Defined function for recoloring boxes according to current time

--- js/script.js
+++ js/script.js
@@ -6,21 +6,36 @@ function showActualTime() {
     $('#currentDay').text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 }
 
+showActualTime();
 setInterval(showActualTime, 1000);
 
 // make boxes color
 //  - based on current time
-//
+// past, present, future classes for colors
+
+var currentHour = moment("13", "H").format("H");
+
 function currentTimeBox() {
-    if (moment().format("HH") > 0) {
-    console.log('ok');
-    
+    for (i = 9; i <= 17; i++)
+        if (currentHour > i) {
+            // console.log('ok');
+            $("#hour-" + i).addClass("past");
+        }
+        else if (currentHour == i) {
+            $("#hour-" + i).addClass("present");
+        }
+        else if (currentHour < i) {
+            $("#hour-" + i).addClass("future");
+        }
     return;
-}};
+    };
 
 setInterval(currentTimeBox(), 1000);
+
 // Make buttons work
 //  -write input to localstorage
+
+
 
 // Figure out local storage pairs
 //  -
