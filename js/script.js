c0a8217cab5155da81c9c86cdfe5c8e9b53405b1
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-03-25 21:26:45 -0700

    inital commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Get current date
+//  - append to header
+// 
+// make boxes color
+//  - based on current time
+//
+// Make buttons work
+//  -write input to localstorage
+// 
+// Figure out local storage pairs
+//  -
+// 
+// 
+// 
+// 
+// 
+// 
+// 
